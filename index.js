a1f7048d0b46c272891105a7750f68427cca30ed
OdiazPC <36682421+djjelly@users.noreply.github.com>
2018-08-16 16:22:28 -0700

    added files

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let request = require('request');
+
+let apiKey = 'ae736100e421c3b4403353e614e30b6c';
+let city = 'Seattle';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
